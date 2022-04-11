2dec7879a8b3ba362177f23495729f926bfbab37
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-11 15:24:06 +0530

    final commit

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Employee = require('../models/employee');
-const ObjecId = require('mongoose').Types.ObjecId;
+const ObjectId = require('mongoose').Types.ObjectId;
 
+//get api
 router.get('/',(req,res)=>{
     Employee.find( (err,doc)=>{
         if(err){
@@ -15,8 +16,9 @@ router.get('/',(req,res)=>{
 });
 
 
+//get api by id
 router.get('/:id',(req,res)=>{
-    if(ObjecId.isValid(req.params.id)){
+    if(ObjectId.isValid(req.params.id)){
         Employee.findById(req.params.id,(err,doc)=>{
             if(err){
                 console.log("Error in get data with id:"+ err);
@@ -29,8 +31,7 @@ router.get('/:id',(req,res)=>{
     }
 });
 
-
-
+//post api by id
 router.post('/',(req,res)=>{
     let emp=new Employee({
         name:req.body.name,
@@ -47,6 +48,44 @@ router.post('/',(req,res)=>{
         }
     })
 })
+
+//put api by id
+router.put('/:id',(req,res)=>{
+    if(ObjectId.isValid(req.params.id)){
+        
+        let emp={
+        name:req.body.name,
+        age:req.body.age,
+        dept:req.body.dept,
+        experience:req.body.experience
+    };
+        Employee.findByIdAndUpdate(req.params.id,{$set:emp},{new:true},(err,doc)=>{
+            if(err){
+                console.log("Error in updating data with id:"+ err);
+            }else{
+                res.send(doc);
+            }
+        });
+    }else{
+        return res.status(400).send('No record found with given id:'+ req.params.id);
+    }
+});
+
+
+//delete api by id
+router.delete('/:id',(req,res)=>{
+    if(ObjectId.isValid(req.params.id)){
+        Employee.findByIdAndDelete(req.params.id,(err,doc)=>{
+            if(err){
+                console.log("Error in deleting data with id:"+ err);
+            }else{
+                res.send(doc);
+            }
+        });
+    }else{
+        return res.status(400).send('No record found with given id:'+ req.params.id);
+    }
+});
 
 
 module.exports=router;
